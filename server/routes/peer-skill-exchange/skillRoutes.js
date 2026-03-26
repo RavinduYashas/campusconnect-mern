344698c3db41a26d091afe7675482feae48724ea
RavinduYashas <160612635+RavinduYashas@users.noreply.github.com>
2026-03-26 15:58:33 +0530

    skillRout

--- server/routes/peer-skill-exchange/skillRoutes.js
+++ server/routes/peer-skill-exchange/skillRoutes.js
@@ -5,12 +5,8 @@ const {
     getAllSkills,
     getSkillById,
     updateSkill,
-<<<<<<< HEAD
     deleteSkill,
     replyToSkillRequest
-=======
-    deleteSkill
->>>>>>> 9b0a3de
 } = require('../../controllers/peer-skill-exchange/skillController');
 const { protect } = require('../../middleware/authMiddleware');
 
@@ -18,11 +14,8 @@ router.route('/')
     .post(protect, createSkill)
     .get(getAllSkills);
 
-<<<<<<< HEAD
 router.post('/reply/:id', protect, replyToSkillRequest);
 
-=======
->>>>>>> 9b0a3de
 router.route('/:id')
     .get(getSkillById)
     .put(protect, updateSkill)
